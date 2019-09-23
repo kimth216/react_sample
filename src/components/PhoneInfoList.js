636bfd481ba8a6036695400e49b6bfc879e3c9e6
kth216 <kth216@imform.co.kr>
2019-09-23 09:06:28 +0900

    init commit

--- src/components/PhoneInfoList.js
+++ src/components/PhoneInfoList.js
@@ -1,16 +1,51 @@
-// src/components/PhoneInfoList.js
 import React, { Component } from 'react';
 import PhoneInfo from './PhoneInfo';
 
+const sample_Array = () =>{
+  // YES
+  const array = [1,2,3,4];
+  const differentArray = [...array, 5];
+  // 혹은 = array.concat(5)
+  console.log(array !== differentArray); // true
+};
+
+const sample_Object = () =>{
+// YES
+  const object = {
+    foo: 'hello',
+    bar: 'world'
+  };
+  const differentObject = {
+    ...object,
+    baz: 'bye'
+  };
+  console.log(differentObject !== object); // true
+};
+
+
+
 class PhoneInfoList extends Component {
   static defaultProps = {
-    data: []
-  }
+    data: [],
+    onRemove: () => console.warn('onRemove not defined'),
+    onUpdate: () => console.warn('onUpdate not defined'),
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.data !== this.props.data;
+  };
 
   render() {
-    const { data } = this.props;
+    console.log('render PhoneInfoList');
+    const { data, onRemove, onUpdate } = this.props;
     const list = data.map(
-      info => (<PhoneInfo key={info.id} info={info}/>)
+      info => (
+        <PhoneInfo
+          key={info.id}
+          info={info}
+          onRemove={onRemove}
+          onUpdate={onUpdate}
+        />)
     );
 
     return (
